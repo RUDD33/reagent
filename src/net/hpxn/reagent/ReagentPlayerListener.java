b6ca0bcb2f82e8f3bb607bede5e63880ce073263
pr0f1x <peterdi+github@gmail.com>
2011-04-26 17:23:20 -0400

    initial import

---
+++ src/net/hpxn/reagent/ReagentPlayerListener.java
@@ -0,0 +1,274 @@
+package net.hpxn.reagent;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.util.config.Configuration;
+
+public class ReagentPlayerListener extends PlayerListener {
+	protected static final Logger log = Logger.getLogger( "Minecraft" );
+	private final ReagentPlugin rp;
+	private Configuration config;
+	private final int MAX_HEALTH = 20;
+	private final int MAX_DISTANCE = 120;
+
+	public ReagentPlayerListener( ReagentPlugin rp ) {
+		this.rp = rp;
+	}
+
+	public void onPlayerInteract( PlayerInteractEvent event ) {
+		if ( (event.getAction() == Action.RIGHT_CLICK_BLOCK)
+						|| (event.getAction() == Action.RIGHT_CLICK_AIR) ) {
+			Player player = event.getPlayer();
+			try {
+				String wSpell = rp.playerSpellMap.get( player );
+				if ( wSpell != null ) {
+					Method wMethod =
+						this.getClass().getMethod( wSpell, Player.class );
+					boolean isSuccess = (Boolean) wMethod.invoke( this, player );
+
+					if ( isSuccess ) {
+						if ( config.getBoolean( "broadcast", true ) ) {
+							rp.getServer().broadcastMessage(
+								player.getName() + " cast " + wSpell + "!" );
+						} else {
+							player.sendMessage( "You cast " + wSpell + "!" );
+						}
+						rp.playerSpellMap.remove( player );
+					} else {
+						player.sendMessage( "fizzle..." );
+					}
+				}
+			} catch ( SecurityException e ) {
+				log.severe( "Reagent: " + e.getMessage() );
+			} catch ( IllegalArgumentException e ) {
+				log.severe( "Reagent: " + e.getMessage() );
+			} catch ( NoSuchMethodException e ) {
+				player.sendMessage( "Unknown spell..." );
+			} catch ( IllegalAccessException e ) {
+				log.severe( "Reagent: " + e.getMessage() );
+			} catch ( InvocationTargetException e ) {
+				log.severe( "Reagent: " + e.getMessage() );
+			}
+		}
+	}
+
+	public Configuration getConfig() {
+		return config;
+	}
+
+	public void setConfig( Configuration config ) {
+		this.config = config;
+	}
+
+	/**
+	 * Casts the timetravel spell. This moves time forward 1 hour.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean timetravel( Player player ) {
+		// TODO timetravel.
+		return true;
+	}
+
+	/**
+	 * Casts the teleportspawn spell. This teleports the player to their spawn
+	 * location.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean teleportspawn( Player player ) {
+		player.teleport( player.getCompassTarget() );
+		return true;
+	}
+
+	/**
+	 * Casts the air spell. This replenishes your air supply while underwater.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean air( Player player ) {
+		player.setMaximumAir( 20 );
+		player.setRemainingAir( 20 );
+		return true;
+	}
+
+	/**
+	 * Casts the heal spell. This heals your character completely.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean heal( Player player ) {
+		player.setHealth( MAX_HEALTH );
+		return true;
+	}
+
+	/**
+	 * Casts the arrowstorm spell. This shoots 5 arrows simultaneously. This
+	 * spell might suck.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean arrowstorm( Player player ) {
+		for ( int x = 0; x < 5; x++ ) {
+			player.shootArrow();
+		}
+		return true;
+	}
+
+	/**
+	 * Casts the skeleton spell. This spell spawns a skeleton at a maximum of 20
+	 * blocks from the player location. Not much use since it will probably
+	 * attack you.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean skeleton( Player player ) {
+		Block wTargetBlock =
+			player.getTargetBlock( null, 20 ).getFace( BlockFace.UP );
+		player.getWorld().spawnCreature( wTargetBlock.getLocation(),
+			CreatureType.SKELETON );
+		return true;
+	}
+
+	public boolean stonewall( Player player ) {
+		Block wTargetBlock = player.getTargetBlock( null, 20 );
+		double wTargetX = wTargetBlock.getX();
+		double wTargetY = wTargetBlock.getY();
+
+		double wPlayerX = player.getLocation().getX();
+		double wPlayerY = player.getLocation().getY();
+
+		double wDeltaX = wTargetX - wPlayerX;
+		double wDeltaY = wTargetY - wPlayerY;
+
+		double wTheta = Math.atan2( wDeltaY, wDeltaX );
+
+		double wDegrees = wTheta * (180 / Math.PI);
+
+		player.sendMessage( "Degrees: " + wDegrees );
+
+		return true;
+	}
+
+	public boolean firewall( Player player ) {
+		// TODO firewall.
+		return true;
+	}
+
+	/**
+	 * Casts the teleport spell. Player will be teleported to the selected cube.
+	 * Maximum teleport distance of MAX_DISTANCE squares. The target block must
+	 * have at least 2 air blocks above it or the spell will fizzle.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean teleport( Player player ) {
+		Block wTargetBlock = player.getTargetBlock( null, MAX_DISTANCE );
+		Block wOneAbove = wTargetBlock.getRelative( BlockFace.UP );
+		Block wTwoAbove = wOneAbove.getRelative( BlockFace.UP );
+		if ( wOneAbove.getType() == Material.AIR
+						&& wTwoAbove.getType() == Material.AIR ) {
+			player.teleport( wTargetBlock.getFace( BlockFace.UP ).getLocation() );
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Casts the bolt spell. 1 square of lightning will hit the ground.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean bolt( Player player ) {
+		Location wLocation =
+			player.getTargetBlock( null, MAX_DISTANCE ).getLocation();
+		player.getWorld().strikeLightning( wLocation );
+		return true;
+	}
+
+	/**
+	 * Casts the fire spell. 3x3 grid of fire.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean fire( Player player ) {
+		Block wTarget = player.getTargetBlock( null, MAX_DISTANCE );
+		wTarget.getFace( BlockFace.UP ).setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.EAST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.WEST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.NORTH )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.SOUTH )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.NORTH_EAST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.NORTH_WEST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.SOUTH_EAST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		wTarget.getRelative( BlockFace.SOUTH_WEST )
+						.getFace( BlockFace.UP )
+						.setType( Material.FIRE );
+		return true;
+	}
+
+	/**
+	 * Casts the precip spell. This is called precip because the precipitation
+	 * type depends on the biome the player is currently in when the spell is
+	 * cast.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean precip( Player player ) {
+		player.getWorld().setStorm( true );
+		player.getWorld().setThundering( true );
+		player.getWorld().setThunderDuration( 100 );
+		player.getWorld().setWeatherDuration( 500 );
+		return true;
+	}
+
+	/**
+	 * Casts the timebomb spell. This kind of a hack because there does not seem
+	 * to be a way to actually make a block explode. This will change the
+	 * targeted block into TNT then light the TNT on fire.
+	 * 
+	 * @param player
+	 * @return true if success
+	 */
+	public boolean timebomb( Player player ) {
+		Block wTargetBlock = player.getTargetBlock( null, MAX_DISTANCE );
+		wTargetBlock.setTypeId( 46, false );
+		wTargetBlock.getFace( BlockFace.UP ).setType( Material.FIRE );
+		return true;
+	}
+}
---
+++ src/net/hpxn/reagent/ReagentPlugin.java
@@ -0,0 +1,154 @@
+package net.hpxn.reagent;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class ReagentPlugin extends JavaPlugin {
+
+	protected static final Logger log = Logger.getLogger( "Minecraft" );
+	private Configuration config;
+	private PluginDescriptionFile pdf;
+	private final ReagentPlayerListener pLst = new ReagentPlayerListener( this );
+	public ConcurrentHashMap<Player, String> playerSpellMap;
+
+	public void onDisable() {
+		log.info( pdf.getName() + " v" + pdf.getVersion() + " - Disabled." );
+	}
+
+	public void onEnable() {
+		config = getConfiguration();
+		pdf = getDescription();
+
+		pLst.setConfig( config );
+		playerSpellMap = new ConcurrentHashMap<Player, String>();
+
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent( Event.Type.PLAYER_INTERACT, pLst, Priority.Normal,
+			this );
+
+		log.info( pdf.getName() + " v" + pdf.getVersion() + " - Enabled." );
+	}
+
+	@Override
+	public boolean onCommand( CommandSender sender, Command cmd, String label,
+					String[] args ) {
+		if ( !cmd.getName().equalsIgnoreCase( "reagent" ) ) {
+			return false;
+		}
+		Player player = null;
+		if ( sender instanceof Player ) {
+			player = (Player) sender;
+		}
+		if ( args.length > 0 ) {
+			String spell = args[0];
+			if ( isSpellAvailable( spell ) ) {
+				player.sendMessage( "Unknown spell..." );
+				return true;
+			}
+
+			if ( hasMaterials( player, spell, true ) ) {
+				playerSpellMap.put( player, spell );
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Check if a spell has been configured in the config.yml. If not then the
+	 * spell is disabled.
+	 * 
+	 * @param spell
+	 * @return true if spell is in config.yml. false otherwise.
+	 */
+	private boolean isSpellAvailable( String spell ) {
+		if ( config.getProperty( "spells." + spell ) == null ) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Checks if a player has the required spell materials in their inventory.
+	 * If the remove boolean is true then the items will also be removed.
+	 * 
+	 * @param player
+	 * @param spell
+	 * @param remove
+	 * @return true if player has all required materials. false otherwise.
+	 */
+	private boolean hasMaterials( Player player, String spell, boolean remove ) {
+		for ( Entry<?, ?> wMaterialsCost : ((Map<?, ?>) config.getProperty( "spells."
+						+ spell )).entrySet() ) {
+			Material wMaterial =
+				Material.valueOf( ((String) wMaterialsCost.getKey()).toUpperCase() );
+			Integer wCost = (Integer) wMaterialsCost.getValue();
+			if ( !player.getInventory().contains( wMaterial, wCost ) ) {
+				player.sendMessage( "Not enough materials to cast " + spell
+								+ "." );
+				if ( config.getBoolean( "hint", false ) ) {
+					player.sendMessage( "Missing "
+									+ wMaterial.name().toLowerCase() + "." );
+				}
+				return false;
+			}
+		}
+		if ( remove ) {
+			removeMaterials( player, spell );
+		}
+		return true;
+	}
+
+	/**
+	 * Removes items from a players inventory based on spell type.
+	 * 
+	 * @param player
+	 * @param spell
+	 */
+	private void removeMaterials( Player player, String spell ) {
+		String wSpellcost = "";
+		for ( Entry<?, ?> wMtlsAmt : ((Map<?, ?>) config.getProperty( "spells."
+						+ spell )).entrySet() ) {
+			Material wMtl =
+				Material.valueOf( ((String) wMtlsAmt.getKey()).toUpperCase() );
+			Integer wAmt = (Integer) wMtlsAmt.getValue();
+			removeItems( player, wMtl, wAmt );
+			wSpellcost += wAmt + " " + wMtl.name().toLowerCase() + " ";
+		}
+		wSpellcost = wSpellcost.replace( '_', ' ' );
+		player.sendMessage( spell + " spell ready! " + wSpellcost + "consumed." );
+	}
+
+	/**
+	 * Removes item(s) from a players inventory.
+	 * 
+	 * @param player
+	 * @param material
+	 * @param amount
+	 */
+	private void removeItems( Player player, Material material, int amount ) {
+		for ( ItemStack wItemStack : player.getInventory().getContents() ) {
+			if ( wItemStack != null && wItemStack.getType() == material ) {
+				if ( wItemStack.getAmount() == amount ) {
+					player.getInventory().remove( wItemStack );
+				} else if ( wItemStack.getAmount() > amount ) {
+					wItemStack.setAmount( wItemStack.getAmount() - amount );
+				}
+			}
+		}
+	}
+}
